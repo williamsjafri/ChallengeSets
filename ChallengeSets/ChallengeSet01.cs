c10a01242747bcf9db8bf13a6fdf981309047e70
William Sjafri <sjafriwilliam@gmail.com>
2020-10-22 19:03:05 -0700

    finished twonumberthesame and subtract

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -10,7 +10,7 @@ namespace ChallengeSets
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend-subtrahend;
         }
 
         public int Add(int number1, int number2)
