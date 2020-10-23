5494314673466ae506ae81a96a86d5506881b6e3
William Sjafri <sjafriwilliam@gmail.com>
2020-10-22 19:20:35 -0700

    finished multiply

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -36,7 +36,7 @@ namespace ChallengeSets
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
