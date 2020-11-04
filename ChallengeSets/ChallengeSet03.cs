db3e3267a89a28228f9081a31aa83f9a009f36fc
William Sjafri <sjafriwilliam@gmail.com>
2020-11-04 09:29:33 -0800

    finished divide

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -76,7 +76,11 @@ namespace ChallengeSets
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor==0)
+            {
+                return 0;
+            }
+            return dividend/divisor;
         }
 
         public int LastMinusFirst(int[] nums)
