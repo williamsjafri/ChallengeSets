360c33bca45d335deef4733d92fe8cfa0522b45b
William Sjafri <sjafriwilliam@gmail.com>
2021-01-08 18:38:58 -0800

    finished set 6 and nth element

--- ChallengeSets/ChallengeSet06.cs
+++ ChallengeSets/ChallengeSet06.cs
@@ -109,7 +109,29 @@ namespace ChallengeSets
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> nthElement = new List<double>();
+            double[] nullCheck = new double[0];
+            if (elements == null)
+            {
+                return nullCheck;
+
+            }
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % n==0)
+                {
+                    nthElement.Add(elements[i]);
+                }
+                if (n<0||n>elements.Count)
+            {
+                nthElement.Clear();
+            }
+                
+                
+            }
+            
+            double[] finalArray = nthElement.ToArray();
+            return finalArray;
         }
     }
 }
